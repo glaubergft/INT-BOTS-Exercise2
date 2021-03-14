2b16d76ba211bb2e9a0d9cd91733253d8e28313c
Glauber Torres <glauber_sp@yahoo.com>
2021-03-14 19:33:29 -0400

    Implementing unit testing

--- Assets/Tests/EditMode/PlayerTests.cs
+++ Assets/Tests/EditMode/PlayerTests.cs
@@ -12,13 +12,13 @@ namespace Tests
         Player player;
         Image healthBar;
 
-        [SetUp]
-        public void Setup()
-        {
-            player = GameObject.Find("Player").GetComponent<Player>();
-            healthBar = GameObject.Find("HealthBar").GetComponent<Image>();
-            player.Awake();
-        }
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    player = GameObject.Find("Player").GetComponent<Player>();
+        //    healthBar = GameObject.Find("HealthBar").GetComponent<Image>();
+        //    player.Awake();
+        //}
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
@@ -33,15 +33,15 @@ namespace Tests
             yield return null;
         }
 
-        [UnityTest]
-        public IEnumerator ReceiveHeadShotTest()
-        {
-            float initialHealthAmount = healthBar.fillAmount;
-            var projectileObj = new Projectile();
-            player.TakeHit(projectileObj, HitType.Head);
-            float currentHealthAmount = healthBar.fillAmount;
-            Assert.AreEqual(0.7f, currentHealthAmount, "Health should be decreased by 30%");
-            yield return null; 
-        }
+        //[UnityTest]
+        //public IEnumerator ReceiveHeadShotTest()
+        //{
+        //    float initialHealthAmount = healthBar.fillAmount;
+        //    var projectileObj = new Projectile();
+        //    player.TakeHit(projectileObj, HitType.Head);
+        //    float currentHealthAmount = healthBar.fillAmount;
+        //    Assert.AreEqual(0.7f, currentHealthAmount, "Health should be decreased by 30%");
+        //    yield return null; 
+        //}
     }
 }
